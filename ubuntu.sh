76dad5a59974d6e1629112cd8a1cc6c1607efd32
Jason Turner <jason@emptycrate.com>
2022-05-05 17:50:49 -0600

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,13 @@
+sudo apt update -y
+
+sudo apt upgrade -y
+
+sudo apt install -y jq g++-11 git python3 python3-pip cppcheck clang-tidy-13 clang-tidy ccache moreutils
+
+pip install conan ninja cmake
+
+sudo snap install code --classic
+
+code --install-extension ms-vscode.cpptools-extension-pack --install-extension jeff-hykin.better-cpp-syntax --install-extension eamodio.gitlens --install-extension jdinhlife.gruvbox
+
+test -f ~/.config/Code/User/settings.json && jq -r '."terminal.integrated.minimumContrastRatio" |= 1' ~/.config/Code/User/settings.json | sponge ~/.config/Code/User/settings.json || echo "{ \"terminal.integrated.minimumContrastRatio\": 1 }" > ~/.config/Code/User/settings.json
