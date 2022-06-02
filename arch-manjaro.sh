6528b10741754b0ee088ee71167a2233424d2203
Jason Turner <jason@emptycrate.com>
2022-06-01 20:26:24 -0600

    use AUR packages to make sure all code extensions can be added

--- arch-manjaro.sh
+++ arch-manjaro.sh
@@ -1,8 +1,20 @@
 read -p "Press enter to install C++ related tools, ctrl-c to cancel" x
 
 
-sudo pacman -Syu gcc git python-pip cppcheck clang ccache vscode cmake ninja vscode
+sudo pacman -Syu --needed gcc git python-pip cppcheck clang ccache vscode cmake ninja vscode
 pip install --user conan
+
+
+git clone https://aur.archlinux.org/code-features.git
+cd code-features
+makepkg -si --noconfirm
+cd ..
+
+git clone https://aur.archlinux.org/code-marketplace.git
+cd code-marketplace
+makepkg -si --noconfirm
+cd ..
+
 
 code --install-extension ms-vscode.cpptools-extension-pack --install-extension jeff-hykin.better-cpp-syntax --install-extension eamodio.gitlens --install-extension jdinhlife.gruvbox
 
