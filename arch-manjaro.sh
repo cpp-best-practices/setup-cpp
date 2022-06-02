48d0af4e45c10cae89234c85b8ace6f0e0559dda
Jason Turner <jason@emptycrate.com>
2022-06-01 19:05:03 -0600

    Update arch-manjaro.sh

--- arch-manjaro.sh
+++ arch-manjaro.sh
@@ -1,7 +1,8 @@
 read -p "Press enter to install C++ related tools, ctrl-c to cancel" x
 
 
-sudo pacman -Syu g++ git python-pip cppcheck clang-tidy ccache vscode conan cmake ninja vscode
+sudo pacman -Syu g++ git python-pip cppcheck clang-tidy ccache vscode cmake ninja vscode
+pip install --user conan
 
 code --install-extension ms-vscode.cpptools-extension-pack --install-extension jeff-hykin.better-cpp-syntax --install-extension eamodio.gitlens --install-extension jdinhlife.gruvbox
 
