26153cdb4f8ebb98c059e70caa11fbb74e0618b8
Jason Turner <jason@emptycrate.com>
2022-06-01 18:56:08 -0600

    small fixes for ubuntu script

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,4 @@
-read "Press enter to install C++ related tools, ctrl-c to cancel" x
+read -p "Press enter to install C++ related tools, ctrl-c to cancel" x
 
 sudo apt update -y
 
@@ -9,7 +9,9 @@ sudo apt install -y jq g++-11 git python3 python3-pip cppcheck clang-tidy-13 cla
 pip install --user conan ninja cmake
 
 echo "Installing vscode via snap or straight from deb package if necessary"
-sudo snap install code --classic || wget https://code.visualstudio.com/sha/download?build=stable\&os=linux-deb-arm64 -O vscode.deb && sudo dpkg -i vscode.deb
+sudo snap install code --classic || (wget https://code.visualstudio.com/sha/download?build=stable\&os=linux-deb-arm64 -O vscode.deb && sudo dpkg -i vscode.deb)
+
+sudo apt -f install
 
 code --install-extension ms-vscode.cpptools-extension-pack --install-extension jeff-hykin.better-cpp-syntax --install-extension eamodio.gitlens --install-extension jdinhlife.gruvbox
 
