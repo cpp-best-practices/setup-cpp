d7796989f284449df6f95e6fbd30bdb3dede5964
Jason Turner <jason@emptycrate.com>
2022-06-10 13:27:54 -0600

    Add clang-format for ubuntu script

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ sudo apt update -y
 
 sudo apt upgrade -y
 
-sudo apt install -y jq g++-11 git python3 python3-pip cppcheck clang-tidy-13 clang-tidy ccache moreutils
+sudo apt install -y jq g++-11 git python3 python3-pip cppcheck clang-tidy-13 clang-tidy clang-format ccache moreutils
 
 pip install --user conan ninja cmake
 
@@ -13,6 +13,6 @@ sudo snap install code --classic || (wget https://code.visualstudio.com/sha/down
 
 sudo apt -f install
 
-code --install-extension ms-vscode.cpptools-extension-pack --install-extension jeff-hykin.better-cpp-syntax --install-extension eamodio.gitlens --install-extension jdinhlife.gruvbox
+code --install-extension ms-vscode.cpptools-extension-pack --install-extension jeff-hykin.better-cpp-syntax --install-extension eamodio.gitlens --install-extension jdinhlife.gruvbox --install-extension xaver.clang-format
 
 test -f ~/.config/Code/User/settings.json && jq -r '."terminal.integrated.minimumContrastRatio" |= 1' ~/.config/Code/User/settings.json | sponge ~/.config/Code/User/settings.json || echo "{ \"terminal.integrated.minimumContrastRatio\": 1 }" > ~/.config/Code/User/settings.json
